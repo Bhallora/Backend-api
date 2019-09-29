85c72a5e3991d129054d353eedaabcd4b23697f7
Pallab Borah <pallab.bora100@gmail.com>
2019-09-29 00:33:17 -0700

    connection to db

--- routes/EmpoyeeRoute.js
+++ routes/EmpoyeeRoute.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+/* configuring password and username for mongodb*/
+const DB_USER= 'Bhallora'
+const DB_USER_PASSWORD = "786786"
+const DB_URL =`mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0-pi5qk.azure.mongodb.net/test?retryWrites=true&w=majority`;
+
 
 //use of express.Router()//
 const router = express.Router();
@@ -20,6 +26,33 @@ router.use(bodyParser.json());
 let employeesArray= employees;
 
 
+/*Establishing connection to the database*/
+mongoose.connect(DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open',()=>{
+    console.log('Hooray we connected to mongodb');
+});
+
+/* schema definition */
+
+const EmployeeSchema = mongoose.Schema({
+
+    _id: mongoose.Schema.Types.ObjectId,  //for unique IDs.
+     name: String,
+     employeeCode: Number,
+     voucherNo: String,
+     date: String,
+     department: String,
+     division: String,
+     expenseDate: String,
+     expenseDetail: String,
+     amount: Number,
+
+});
+const EmployeeModel=mongoose.model('Employee', EmployeeSchema);
+
+
+
 //get request
 router.get('/', (req, res) => {
     res.json(employeesArray);
@@ -38,11 +71,24 @@ router.get('/:id', (req, res) => {
 
 //post request
 router.post('/', (req,res)=>{
-    console.log("handling POST request");
+   /* console.log("handling POST request");
     console.log(req.body);
     //validation can be done while pushing
     employeesArray.push(req.body);
-    res.sendStatus(200);
+    res.sendStatus(200);*/
+    const id = new mongoose.Types.ObjectId();
+    const employeeToPersist = Object.assign({
+        _id: id}, req.body
+    );
+    const employee= new EmployeeModel(employeeToPersist);
+    employee.save().then ((err, employee) => {
+    
+        if (err) res.status(500).send(err);
+        res.json(employee);
+    }); 
+
+    console.log(JSON.stringify(employeeToPersist));
+    //res.end();
 });
 
 //advantage of using router, param method used below helps in validation //
